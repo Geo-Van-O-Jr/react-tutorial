968acb394dd0699604a27cb1d82d709bb57c6c4b
Geovano Amaro <geovanejr23@oulook.com>
2022-10-14 20:02:07 -0300

    learning hooks

--- src/Components/planets/planet/index.js
+++ src/Components/planets/planet/index.js
@@ -2,22 +2,23 @@ import React from "react";
 import GreyImg from "../../shared/grey-img";
 import DescriptionWithLink from "../../shared/grey-img/DescriptionWithLink";
 
-async function getSatellites(id){
-    let response = await fetch(`http://localhost:300/api/${id}.json`)
+async function getSatellites(id) {
+    let response = await fetch(`http://localhost:3000/api/${id}.json`)
     let data = await response.json()
+    return data;
 }
 
 
 
 class Planet extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             satellites: []
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         getSatellites(this.props.id).then(data => {
             this.setState(state => ({
                 satellites: ['satellites']
@@ -25,31 +26,31 @@ class Planet extends React.Component {
         })
     }
 
-    render() {   
+    render() {
         let title;
-        if (this.props.title_with_underline) 
+        if (this.props.title_with_underline)
             title = <h4><u>{this.props.name}</u></h4>
         else
             title = <h4>{this.props.name}</h4>
 
 
         return (
-            <div>                 
+            <div>
                 {title}
                 <DescriptionWithLink description={this.props.description} link={this.props.link} link_description={this.props.link_description} />
                 <GreyImg img_url={this.props.img_url} gray={this.props.gray} />
                 <h4>Satélites</h4>
                 <ul>
-                    {this.state.satellites.map((satellite, index) => 
+                    {this.state.satellites.map((satellite, index) =>
                         <li key={index}>{satellite.name}</li>
                     )}
                 </ul>
                 <hr></hr>
-            
+
 
             </div>
         )
     }
-   }
+}
 
 export default Planet
